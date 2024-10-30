5274c9add2d4274077e289c27d9a21d04c43aca8
Louis Vela <lvela@doximity.com>
2024-10-29 19:47:35 -0500

    Refactor to make one call for search query

--- src/agent.rb
+++ src/agent.rb
@@ -11,45 +11,48 @@ class Agent
   CREATE_STORY_PROMPT = 'Please create a spooky Halloween story based on the following content. ' \
     'Keep it 3 paragraphs. Do not use more that 300 tokens.\n\n'
 
-  def initialize(claude_key: nil, openai_key: nil)
+  def initialize(claude_key: nil, openai_key: nil, query: nil)
     @claude_client = ClaudeClient.new(claude_key)
     @openai_client = OpenAIClient.new(openai_key)
+    @query = query
 
     raise 'API key is required.' if @claude_client.nil? || @openai_client.nil?
   end
 
-  def summarize(query, num_results: 3)
-    content = WebSearcher.new.search(query, num_results:)
-
+  def summarize
     summary_prompt = "#{SUMMARIZE_PROMPT}<content>#{content}</content>"
 
     # Generate summaries based on specified provider
     summaries = {}
     puts '-----------------'
-    puts "Running claude prompt: #{summary_prompt[0..350]}..."
+    puts "Running claude prompt: #{summary_prompt[0..550]}..."
     summaries[:claude] = @claude_client.send(summary_prompt)
 
     puts '-----------------'
-    puts "Running openai prompt: #{summary_prompt[0..350]}..."
+    puts "Running openai prompt: #{summary_prompt[0..550]}..."
     summaries[:openai] = @openai_client.send(summary_prompt)
 
     summaries
   end
 
-  def create_stories(query, num_results: 3)
-    content = WebSearcher.new.search(query, num_results:)
-
+  def create_stories
     create_story_prompt = "#{CREATE_STORY_PROMPT}<content>#{content}</content>"
 
     stories = {}
     puts '-----------------'
-    puts "Running claude prompt: #{create_story_prompt[0..350]}..."
+    puts "Running claude prompt: #{create_story_prompt[0..550]}..."
     stories[:claude] = @claude_client.send(create_story_prompt)
 
     puts '-----------------'
-    puts "Running openai prompt: #{create_story_prompt[0..350]}..."
+    puts "Running openai prompt: #{create_story_prompt[0..550]}..."
     stories[:openai] = @openai_client.send(create_story_prompt)
 
     stories
+  end
+
+  private
+
+  def content
+    @content ||= WebSearcher.new.search(@query, num_results: 3)
   end
 end
